d67b79a88d9aff0b03b9726ed34dc9941652726f
Daniel Albohn <dna5021@psu.edu>
2020-01-03 23:29:59 -0500

    updated color histogram

--- inst/python/face_features.py
+++ inst/python/face_features.py
@@ -1,12 +1,36 @@
 import cv2
 import mahotas as mt
+import numpy as np
   
 def face_hist(img, shape):
   image = cv2.imread(img)
   
-  hist = cv2.calcHist([image], [0, 1, 2], None, shape, [0, 256, 0, 256, 0, 256])
-  hist = hist.flatten()
-  return hist
+  # grab the image channels, initialize the tuple of colors,
+  # the figure and the flattened feature vector
+  chans = cv2.split(image)
+  colors = ("b", "g", "r")
+  # plt.figure()
+  # plt.title("'Flattened' Color Histogram")
+  # plt.xlabel("Bins")
+  # plt.ylabel("# of Pixels")
+  features = []
+ 
+  # loop over the image channels
+  for (chan, color) in zip(chans, colors):
+	  # create a histogram for the current channel and
+	  # concatenate the resulting histograms for each
+	  # channel
+	  hist = cv2.calcHist([chan], [0], None, [shape], [0, 256])
+	  features.extend(hist)
+ 
+	  # plot the histogram
+	  # plt.plot(hist, color = color)
+	  # plt.xlim([0, 256])
+  
+  hist_ret = np.array(features).flatten()
+  # hist = cv2.calcHist([image], [0, 1, 2], None, shape, [0, 256, 0, 256, 0, 256])
+  # hist = hist.flatten()
+  return hist_ret
 
 def face_texture(img):
   image = cv2.imread(img)
